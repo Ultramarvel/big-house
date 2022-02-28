c4eb8cb254883c8fb56a2bcc3a55b40dccd9d3ba
Ultramarvel <100544943+Ultramarvel@users.noreply.github.com>
2022-02-28 23:16:52 +0800

    Add files via upload

---
+++ SayHello.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class SayHello{
+	public static void main(String args[])
+{
+	Scanner reader=new
+Scanner(System.in);
+	System.out.print("Enter your name.");
+	String name=reader.next();
+	System.out.println("欢迎"+name="学习Java语言!");
+}
+
+}
